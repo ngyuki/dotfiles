b3c4b5822dc701a4c90927b76ca8247c40ffe77a
ngyuki <ngyuki.ts@gmail.com>
2014-06-30 09:37:37 +0900

    peco-ssh

--- function/peco.sh
+++ function/peco.sh
@@ -1,13 +1,45 @@
 ################################################################################
 ### peco
 
-hash peco 1> /dev/null 2>&1 && return
+if ! hash peco 1> /dev/null 2>&1; then
 
-function peco()
+    function peco()
+    {
+        if ! type -f peco 1> /dev/null 2>&1 ; then
+            {
+                echo "You should be install peco"
+                echo "  see https://github.com/peco/peco/releases"
+                echo "  Or..."
+                echo "  go get github.com/peco/peco/cmd/peco"
+            } 1>&2
+        else
+            command peco "$@"
+        fi
+    }
+
+fi
+
+function peco-ssh()
 {
-    if ! type -f peco 1> /dev/null 2>&1 ; then
-        echo "install peco ..." 1>&2
-        go get github.com/peco/peco/cmd/peco
+    local input
+    local host
+
+    if [ -t 0 ]; then
+        input=/etc/hosts
+    else
+        input=-
     fi
-    command peco "$@"
+
+    host=$(
+        cat "$input" |
+        grep -v -e '^$' -e '^[ \t]*#' |
+        sed -e 's/[ \t][ \t]*/\n/g' |
+        sort | uniq | peco
+    )
+
+    if [ -z "$host" ]; then
+        return 1
+    fi
+
+    ssh "$host" "$@"
 }
