3584f28ab78259da523ff6c85881ded73ffef3de
ngyuki <ngyuki.jp@gmail.com>
2017-12-07 12:06:33 +0900

    [function]peco-bind-command の C-x C-f を変更

--- function/peco-bind-command.sh
+++ function/peco-bind-command.sh
@@ -1,39 +1,57 @@
-if [ "${BASH_VERSINFO[0]}" -ge 4 -a -t 1 ]; then
-
-  function __peco_bind_command_replace()
-  {
-    local cmd
-
-    cmd=$(
-      local orig="$(stty -g)"
-      trap "stty \"${orig}\"" EXIT
-      stty cooked echo
-      read -p 'replacing with command: ' -r input
-      echo "$input"
-    )
-
-    if [ -z "$cmd" ]; then
-      return 1
-    fi
-
-
-    local line
-
-    line=$(
-      eval "$cmd" | peco
-    )
-
-    if [ -z "$line" ]; then
-      return 1
-    fi
-
-    local pos=$READLINE_POINT
-    local len=${#READLINE_LINE}
-
-    READLINE_LINE="${READLINE_LINE:0:$pos}${line}${READLINE_LINE:$pos:$len}"
-    READLINE_POINT=$(($pos + ${#line}))
-  }
-
-  bind -r '"\C-x\C-f"'
-  bind -x '"\C-x\C-f":__peco_bind_command_replace'
+if [ "${BASH_VERSINFO[0]}" -lt 4 ]; then
+    return
 fi
+
+function __peco_bind_command_find() {
+
+  local line
+  line=$(
+    find -L "${1:-.}" -path '*/\.*' -prune -o -print 2> /dev/null | __peco
+  )
+
+  if [ -z "$line" ]; then
+    return 1
+  fi
+
+  local pos=$READLINE_POINT
+  local len=${#READLINE_LINE}
+
+  READLINE_LINE="${READLINE_LINE:0:$pos}${line}${READLINE_LINE:$pos:$len}"
+  READLINE_POINT=$(($pos + ${#line}))
+}
+
+function __peco_bind_command_replace() {
+  local cmd
+
+  cmd=$(
+    local orig="$(stty -g)"
+    trap "stty \"${orig}\"" EXIT
+    stty cooked echo
+    read -p 'replacing with command: ' -r input
+    echo "$input"
+  )
+
+  if [ -z "$cmd" ]; then
+    return 1
+  fi
+
+
+  local line
+
+  line=$(
+    eval "$cmd" | __peco
+  )
+
+  if [ -z "$line" ]; then
+    return 1
+  fi
+
+  local pos=$READLINE_POINT
+  local len=${#READLINE_LINE}
+
+  READLINE_LINE="${READLINE_LINE:0:$pos}${line}${READLINE_LINE:$pos:$len}"
+  READLINE_POINT=$(($pos + ${#line}))
+}
+
+bind -r '"\C-x\C-f"'
+bind -x '"\C-x\C-f":__peco_bind_command_find'
