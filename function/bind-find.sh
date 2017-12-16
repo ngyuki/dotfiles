7f6bc720fe3e68952c5be5e1388748e95baf8052
ngyuki <ngyuki.jp@gmail.com>
2017-12-16 09:47:30 +0900

    bind commands

---
+++ function/bind-find.sh
@@ -0,0 +1,42 @@
+################################################################################
+### bind-find
+
+if [ "${BASH_VERSINFO[0]}" -lt 4 ]; then
+  return
+fi
+
+if [ ! -t 1 ]; then
+  return
+fi
+
+function __bind_command_find() {
+
+  local input=$(
+    find -L "${1:-.}" -path '*/\.*' -prune -o -print 2> /dev/null |
+    fzf -m
+  )
+
+  if [ -z "$input" ]; then
+    return 1
+  fi
+
+  local arr=()
+  local line
+  IFS=$'\n' eval '
+  for line in $input; do
+    if [ -n "$line" ]; then
+      arr+=($(printf "%q" "$line"))
+    fi
+  done'
+
+  local input="${arr[@]}"
+
+  local pos=$READLINE_POINT
+  local len=${#READLINE_LINE}
+
+  READLINE_LINE="${READLINE_LINE:0:$pos}${input}${READLINE_LINE:$pos:$len}"
+  READLINE_POINT=$(($pos + ${#input}))
+}
+
+bind -r '"\C-x\C-f"'
+bind -x '"\C-x\C-f":__bind_command_find'
---
+++ function/bind-history.sh
@@ -0,0 +1,30 @@
+################################################################################
+### bind-history
+
+if [ "${BASH_VERSINFO[0]}" -lt 4 ]; then
+  return
+fi
+
+if [ ! -t 1 ]; then
+  return
+fi
+
+function __bind_history() {
+
+  local input=$(
+    HISTTIMEFORMAT= builtin history |
+    sed 's/ *[0-9][0-9]* *//' |
+    awk '!a[$0]++' |
+    fzf +m --tac --query "$READLINE_LINE"
+  )
+
+  if [ -z "$input" ]; then
+    return 1
+  fi
+
+  READLINE_LINE="$input"
+  READLINE_POINT=${#READLINE_LINE}
+}
+
+bind -r '"\C-x\C-x"'
+bind -x '"\C-x\C-x":__bind_history'
