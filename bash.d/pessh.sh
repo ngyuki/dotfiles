979d58f1565cdf3c7a5490e3f37202f4daf5f06d
ngyuki <ngyuki.jp@gmail.com>
2018-01-06 00:03:49 +0900

    pessh で bash_completion がないときのフォールバック

--- bash.d/pessh.sh
+++ bash.d/pessh.sh
@@ -3,24 +3,35 @@
 
 # ruptime が無いと _known_hosts_real が妙に遅いのでダミー
 if ! type -f ruptime 2>/dev/null; then
-    ruptime(){
-        if type -f ruptime 2>/dev/null; then
-            unset -f ruptime
-        fi
-    }
+  ruptime(){
+    if type -f ruptime 2>/dev/null; then
+      unset -f ruptime
+    fi
+  }
 fi
 
 pessh() {
-    # bash_completion の _known_hosts_real を呼ぶ
-    _known_hosts_real -a -- ""
+  local host
 
-    host=$(IFS=$'\n'; echo "${COMPREPLY[*]}" | sort | uniq | __peco)
-    COMPREPLY=()
+  if hash _known_hosts_real 2>/dev/null; then
+    host=$(
+      # bash_completion の _known_hosts_real を呼ぶ
+      _known_hosts_real -a -- ""
+      IFS=$'\n'; echo "${COMPREPLY[*]}" | sort | uniq | fzf
+    )
+  else
+    host=$(
+      cat "/etc/hosts" |
+        grep -v -e '^$' -e '^[ \t]*#' |
+        sed -e 's/[ \t][ \t]*/\n/g' |
+        sort | uniq | fzf
+    )
+  fi
 
-    if [ -z "$host" ]; then
-        return 1
-    fi
+  if [ -z "$host" ]; then
+    return 1
+  fi
 
-    history -s ssh "$host" "$@"
-    ssh "$host" "$@"
+  history -s ssh "$host" "$@"
+  ssh "$host" "$@"
 }
