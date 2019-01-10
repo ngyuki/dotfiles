e22aecc2ad35347086d03ad2e45e3b3d7df68f09
ngyuki <ngyuki.jp@gmail.com>
2019-01-10 13:03:48 +0900

    [fish]pecd でディレクトリのファイル一覧をプレビュー

--- fish/functions/pecd.fish
+++ fish/functions/pecd.fish
@@ -1,6 +1,6 @@
 
 function pecd
-  cat $Z_DATA | sort -t \| -k 3nr | cut -d\| -f1 | string escape | fzf +s | read --local select
+  cat $Z_DATA | sort -t \| -k 3nr | cut -d\| -f1 | string escape | fzf +s --preview 'ls -al --color=always {}' | read --local select
   if [ $select ]
     cd $select
   end
