12fa514f82e525e3b3e979a33dccfb4a038a6c14
ngyuki <ngyuki.jp@gmail.com>
2019-04-12 00:29:52 +0900

    fish の pecd を zz にリネーム

--- fish/functions/zz.fish
+++ fish/functions/zz.fish
@@ -1,5 +1,5 @@
 
-function pecd
+function zz
   cat $Z_DATA | sort -t \| -k 3nr | cut -d\| -f1 | string escape | fzf +s --preview 'ls -al --color=always {}' | read --local select
   if [ $select ]
     cd $select
