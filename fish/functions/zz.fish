6bc95342ae6d0d799aea2bbb002e1ce39dab35e9
ngyuki <ngyuki.jp@gmail.com>
2023-05-09 18:07:06 +0900

    fish zz コマンドは z -l -t を使う

--- fish/functions/zz.fish
+++ fish/functions/zz.fish
@@ -1,6 +1,6 @@
 
 function zz
-  cat $Z_DATA | sort -t \| -k 3nr | cut -d\| -f1 | string escape |
+  z -l -t | awk '{ print $2 }' | string escape |
     fzf +s --preview 'ls -al --color=always {}' |
     read --local select
   if [ $select ]
