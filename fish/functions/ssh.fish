60662de456d7054c03ceff51c7c9e7248562ddf6
ngyuki <ngyuki.jp@gmail.com>
2023-05-18 18:03:44 +0900

    ssh でコマンド実行時に TERM=xterm-256color を設定

--- fish/functions/ssh.fish
+++ fish/functions/ssh.fish
@@ -2,6 +2,8 @@ function ssh
   if test (count $argv) -eq 0
     xssh
   else
+    # tmux で TERM が tmux-256color にされるとターミナルタイトルが接続先にならなくなるので
+    export TERM=xterm-256color
     command ssh $argv
   end
 end
