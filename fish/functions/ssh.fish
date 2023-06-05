3f8d8de7b2b7b0046f70bb41d40ed50bbd8fe562
ngyuki <ngyuki.jp@gmail.com>
2023-06-06 03:54:33 +0900

    fish ssh の ProxyCommand は /bin/sh で実行する

--- fish/functions/ssh.fish
+++ fish/functions/ssh.fish
@@ -4,6 +4,8 @@ function ssh
   else
     # tmux で TERM が tmux-256color にされるとターミナルタイトルが接続先にならなくなるので
     export TERM=xterm-256color
+    # ProxyCommand を /bin/sh で実行するため
+    export SHELL=/bin/sh
     command ssh $argv
   end
 end
