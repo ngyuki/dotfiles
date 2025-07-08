bfd6cdfb6d6935cb25a9454c1d3e85a92be11aff
ngyuki <ngyuki.jp@gmail.com>
2025-07-09 00:43:33 +0900

    .gitconfig -> .config/git/config

--- setup/02-git.sh
+++ setup/02-git.sh
@@ -7,10 +7,6 @@ pp "git config"
 PATH=/usr/bin:/bin:$PATH
 
 (
-  cmd=(git config --global include.path "$PWD/home/files/.gitconfig")
-  echo "${cmd[@]}"
-  "${cmd[@]}"
-
   cmd=(git config --global core.hooksPath "$PWD/git-hooks")
   echo "${cmd[@]}"
   "${cmd[@]}"
