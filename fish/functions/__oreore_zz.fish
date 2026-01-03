8f5fa90e87cec66a37c8859ac5649ebdc7e2fff0
ngyuki <ngyuki.jp@gmail.com>
2026-01-04 04:10:34 +0900

    remove fish __oreore_zz

--- fish/functions/__oreore_zz.fish
+++
@@ -1,9 +0,0 @@
-
-function __oreore_zz
-  z -l -t | awk '{ print $2 }' | string escape |
-    fzf --scheme=path --keep-right --no-sort --preview 'ls -alF --color=always {}' |
-    read --local select
-  if [ $select ]
-    cd $select
-  end
-end
