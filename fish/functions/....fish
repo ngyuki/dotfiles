fdcc11b53d7a539c692498845f21181d782c461d
ngyuki <ngyuki.jp@gmail.com>
2026-03-27 11:22:30 +0900

    fish dotdotdot

--- fish/functions/....fish
+++ fish/functions/....fish
@@ -1,6 +1,9 @@
 # https://github.com/junegunn/fzf/wiki/Examples-(fish)
 function ... -d 'cd backwards'
-  pwd | awk -v RS=/ '/\n/ {exit} {p=p $0 "/"; print p}' | tac | eval (__fzfcmd) +m --select-1 --exit-0 $FZF_BCD_OPTS | read -l result
+  set -l arr (string split / $PWD)
+  for i in (seq (count $arr) -1 2)
+      string join / $arr[1..$i] ""
+  end | eval (__fzfcmd) +m --select-1 --exit-0 $FZF_BCD_OPTS | read -l result
   if test -n "$result"
     cd "$result"
   end
