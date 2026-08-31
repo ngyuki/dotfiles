a1d3cca79ee57831e37ac66cd090bb18a00296a9
ngyuki <ngyuki.jp@gmail.com>
2026-09-01 04:23:27 +0900

    setup/07-services.sh

--- setup/07-services.sh
+++ setup/07-services.sh
@@ -4,7 +4,9 @@ source "$(dirname "$0")/functions.sh"
 pp "systemd services"
 
 arr=(
-  "systemctl --user enable systemd-tmpfiles-clean.timer"
+  "systemctl --user enable --now systemd-tmpfiles-clean.timer"
+  "systemctl --user enable --now ssh-agent.socket"
+  "sudo systemd-tmpfiles --create"
 )
 for cmd in "${arr[@]}"; do
   printf "%s\n" "$cmd"
